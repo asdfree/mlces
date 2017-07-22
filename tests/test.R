7a59e5a5115edc9671c3cb1259d491d641130697
Anthony Damico <ajdamico@gmail.com>
2017-07-22 16:49:10 -0400

    setup automation

--- tests/test.R
+++ tests/test.R
@@ -1,12 +1,75 @@
-machine_specific_replacements <- 
-	list( 
-		
-		# replace the folder path on macnix
-		c( 'path.expand( \"~\" ) , \"MLCES\"' , paste0( '"' , getwd() , '"' ) ) ,
-		
-		# change other things in the script to be run
-		c( "hello" , "howdy" )
-	)
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
-source( lodown::syntaxtractor( "mlces" , replacements = machine_specific_replacements , setup_test = "test" ) , echo = TRUE )
+mlces_df <- readRDS( file.path( getwd() , "mcles1997.rds" ) )
+
+mlces_df <- 
+	transform( 
+		mlces_df , 
+		
+		claimant_relationship_to_policyholder =
+			ifelse( relation == "E" , "covered employee" ,
+			ifelse( relation == "S" , "spouse of covered employee" ,
+			ifelse( relation == "D" , "dependent of covered employee" , NA ) ) ) ,
+			
+		ppo_plan = as.numeric( ppo == 'Y' )
+	)
+	
+nrow( mlces_df )
+
+table( mlces_df[ , "claimant_relationship_to_policyholder" ] , useNA = "always" )
+mean( mlces_df[ , "totpdchg" ] )
+
+tapply(
+	mlces_df[ , "totpdchg" ] ,
+	mlces_df[ , "claimant_relationship_to_policyholder" ] ,
+	mean 
+)
+prop.table( table( mlces_df[ , "patsex" ] ) )
+
+prop.table(
+	table( mlces_df[ , c( "patsex" , "claimant_relationship_to_policyholder" ) ] ) ,
+	margin = 2
+)
+sum( mlces_df[ , "totpdchg" ] )
+
+tapply(
+	mlces_df[ , "totpdchg" ] ,
+	mlces_df[ , "claimant_relationship_to_policyholder" ] ,
+	sum 
+)
+quantile( mlces_df[ , "totpdchg" ] , 0.5 )
+
+tapply(
+	mlces_df[ , "totpdchg" ] ,
+	mlces_df[ , "claimant_relationship_to_policyholder" ] ,
+	quantile ,
+	0.5 
+)
+sub_mlces_df <- subset( mlces_df , ( ( claimyr - patbrtyr ) < 18 ) )
+mean( sub_mlces_df[ , "totpdchg" ] )
+var( mlces_df[ , "totpdchg" ] )
+
+tapply(
+	mlces_df[ , "totpdchg" ] ,
+	mlces_df[ , "claimant_relationship_to_policyholder" ] ,
+	var 
+)
+t.test( totpdchg ~ ppo_plan , mlces_df )
+this_table <- table( mlces_df[ , c( "ppo_plan" , "patsex" ) ] )
+
+chisq.test( this_table )
+glm_result <- 
+	glm( 
+		totpdchg ~ ppo_plan + patsex , 
+		data = mlces_df
+	)
+
+summary( glm_result )
+library(dplyr)
+mlces_tbl <- tbl_df( mlces_df )
+mlces_tbl %>%
+	summarize( mean = mean( totpdchg ) )
+
+mlces_tbl %>%
+	group_by( claimant_relationship_to_policyholder ) %>%
+	summarize( mean = mean( totpdchg ) )
